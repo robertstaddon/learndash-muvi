c774234386f8ec63c4463f74d1af559016676773
Robert Staddon <robertstaddon@gmail.com>
2019-02-11 10:49:13 -0600

    Fix indention

--- learndash-muvi.php
+++ learndash-muvi.php
@@ -18,13 +18,13 @@ if ( ! class_exists( 'LearnDash_Muvi' ) ) :
 */
 class LearnDash_Muvi {
 
-	/**
-	 * The LearnDash_Muvi instance
-	 *
-	 * @access private
-	 * @var object $instance
-	 */
-	private static $instance;       
+    /**
+     * The LearnDash_Muvi instance
+     *
+     * @access private
+     * @var object $instance
+     */
+    private static $instance;       
 
     /**
      * The LearnDash_Muvi_Settings instance
@@ -33,14 +33,14 @@ class LearnDash_Muvi {
      * @var object $settings
      */
     private static $settings;
-	
+    
     /**
      * The LearnDash_Muvi_API instance
      *
      * @access private
      * @var object $api
      */
-	private static $api;
+    private static $api;
 
     /**
      * The LearnDash_Muvi_User instance
@@ -48,94 +48,94 @@ class LearnDash_Muvi {
      * @access private
      * @var object $api
      */
-	private static $muvi_user;
+    private static $muvi_user;
 
-	/**
+    /**
      * The LearnDash_Muvi_REST_Server instance
      *
      * @access private
      * @var object $rest_server
      */
-	private static $rest_server;
+    private static $rest_server;
 
-	/*
-	 * The setting id for the Muvi auth token (set in settings and referenced in the API)
-	 */
-	private static $auth_setting_id = 'learndash-muvi-auth-token';
+    /*
+     * The setting id for the Muvi auth token (set in settings and referenced in the API)
+     */
+    private static $auth_setting_id = 'learndash-muvi-auth-token';
 
-	/**
-	 * Instantiate the main class
-	 *
-	 * This function instantiates the class, initialize all functions and return the object.
-	 * 
-	 * @return object The LearnDash_Muvi instance.
-	 */
-	public static function instance() {
+    /**
+     * Instantiate the main class
+     *
+     * This function instantiates the class, initialize all functions and return the object.
+     * 
+     * @return object The LearnDash_Muvi instance.
+     */
+    public static function instance() {
 
-		if ( ! isset( self::$instance ) && ( ! self::$instance instanceof LearnDash_Muvi ) ) {
+        if ( ! isset( self::$instance ) && ( ! self::$instance instanceof LearnDash_Muvi ) ) {
 
-			self::$instance = new LearnDash_Muvi();
-			self::$instance->setup_constants();
-			
-			add_action( 'plugins_loaded', array( self::$instance, 'load_textdomain' ) );
+            self::$instance = new LearnDash_Muvi();
+            self::$instance->setup_constants();
+            
+            add_action( 'plugins_loaded', array( self::$instance, 'load_textdomain' ) );
 
             add_action( 'admin_init', array( self::$instance, 'require_dependencies' ) );
             
-			self::$instance->includes();
-			
-		}
+            self::$instance->includes();
+            
+        }
 
-		return self::$instance;
-	}
+        return self::$instance;
+    }
      
-	/**
-	 * Function for setting up constants
-	 *
-	 * This function is used to set up constants used throughout the plugin.
-	 */
-	public function setup_constants() {
+    /**
+     * Function for setting up constants
+     *
+     * This function is used to set up constants used throughout the plugin.
+     */
+    public function setup_constants() {
 
-		// Plugin version
-		if ( ! defined( 'LEARNDASH_MUVI_VERSION' ) ) {
-			define( 'LEARNDASH_MUVI_VERSION', '1.0' );
-		}
+        // Plugin version
+        if ( ! defined( 'LEARNDASH_MUVI_VERSION' ) ) {
+            define( 'LEARNDASH_MUVI_VERSION', '1.0' );
+        }
 
-		// Plugin text domain
-		if ( ! defined( 'LEARNDASH_MUVI_DOMAIN' ) ) {
-			define( 'LEARNDASH_MUVI_DOMAIN', 'learndash-muvi' );
-		}
-		
-		// Plugin file
-		if ( ! defined( 'LEARNDASH_MUVI_FILE' ) ) {
-			define( 'LEARNDASH_MUVI_FILE', __FILE__ );
-		}		
+        // Plugin text domain
+        if ( ! defined( 'LEARNDASH_MUVI_DOMAIN' ) ) {
+            define( 'LEARNDASH_MUVI_DOMAIN', 'learndash-muvi' );
+        }
+        
+        // Plugin file
+        if ( ! defined( 'LEARNDASH_MUVI_FILE' ) ) {
+            define( 'LEARNDASH_MUVI_FILE', __FILE__ );
+        }		
 
-		// Plugin folder path
-		if ( ! defined( 'LEARNDASH_MUVI_PLUGIN_PATH' ) ) {
-			define( 'LEARNDASH_MUVI_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
-		}
+        // Plugin folder path
+        if ( ! defined( 'LEARNDASH_MUVI_PLUGIN_PATH' ) ) {
+            define( 'LEARNDASH_MUVI_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+        }
 
-		// Plugin folder URL
-		if ( ! defined( 'LEARNDASH_MUVI_PLUGIN_URL' ) ) {
-			define( 'LEARNDASH_MUVI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-		}
-	}
+        // Plugin folder URL
+        if ( ! defined( 'LEARNDASH_MUVI_PLUGIN_URL' ) ) {
+            define( 'LEARNDASH_MUVI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+        }
+    }
 
-	/**
-	 * Load text domain used for translation
-	 *
-	 * This function loads mo and po files used to translate text strings used throughout the 
-	 * plugin.
-	 */
-	public function load_textdomain() {
+    /**
+     * Load text domain used for translation
+     *
+     * This function loads mo and po files used to translate text strings used throughout the 
+     * plugin.
+     */
+    public function load_textdomain() {
 
-		// Set filter for plugin language directory
-		$lang_dir = dirname( plugin_basename( LEARNDASH_MUVI_FILE ) ) . '/languages/';
-		$lang_dir = apply_filters( 'learndash_muvi_languages_directory', $lang_dir );
+        // Set filter for plugin language directory
+        $lang_dir = dirname( plugin_basename( LEARNDASH_MUVI_FILE ) ) . '/languages/';
+        $lang_dir = apply_filters( 'learndash_muvi_languages_directory', $lang_dir );
 
-		// Load plugin translation file
-		load_plugin_textdomain( LEARNDASH_MUVI_DOMAIN, false, $lang_dir );
-	}     
+        // Load plugin translation file
+        load_plugin_textdomain( LEARNDASH_MUVI_DOMAIN, false, $lang_dir );
+    }     
      
     /**
      * Require dependencies
@@ -158,28 +158,28 @@ class LearnDash_Muvi {
     }
 
      
-	/**
-	 * Includes all necessary PHP files
-	 *
-	 * This function is responsible for including all necessary PHP files.
-	 */
-	public function includes() {		
-		if ( is_admin() ) {
-			include LEARNDASH_MUVI_PLUGIN_PATH . '/includes/admin/class-settings.php';
-			self::$settings = new LearnDash_Muvi_Settings( self::$auth_setting_id );
+    /**
+     * Includes all necessary PHP files
+     *
+     * This function is responsible for including all necessary PHP files.
+     */
+    public function includes() {		
+        if ( is_admin() ) {
+            include LEARNDASH_MUVI_PLUGIN_PATH . '/includes/admin/class-settings.php';
+            self::$settings = new LearnDash_Muvi_Settings( self::$auth_setting_id );
 
-		}
-		
-		include LEARNDASH_MUVI_PLUGIN_PATH . '/includes/class-muvi-api.php';
-		self::$api = new LearnDash_Muvi_API( self::$auth_setting_id );
+        }
+        
+        include LEARNDASH_MUVI_PLUGIN_PATH . '/includes/class-muvi-api.php';
+        self::$api = new LearnDash_Muvi_API( self::$auth_setting_id );
 
-		include LEARNDASH_MUVI_PLUGIN_PATH . '/includes/class-user.php';
-		self::$muvi_user = new LearnDash_Muvi_User( self::$api );
+        include LEARNDASH_MUVI_PLUGIN_PATH . '/includes/class-user.php';
+        self::$muvi_user = new LearnDash_Muvi_User( self::$api );
 
-		include LEARNDASH_MUVI_PLUGIN_PATH . '/includes/class-rest-server.php';
-		self::$rest_server = new LearnDash_Muvi_REST_Server( self::$muvi_user );
+        include LEARNDASH_MUVI_PLUGIN_PATH . '/includes/class-rest-server.php';
+        self::$rest_server = new LearnDash_Muvi_REST_Server( self::$muvi_user );
 
-	}
+    }
 
 }
 
@@ -189,7 +189,7 @@ endif; // End if class exists check
  * The main function for returning instance
  */
 function launch_learndash_muvi() {
-	return LearnDash_Muvi::instance();
+    return LearnDash_Muvi::instance();
 }
 
 // Run plugin
