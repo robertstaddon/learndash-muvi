f3fb6da2e4f45bc992f94921de77dd05b7df69f8
Robert Staddon <robertstaddon@gmail.com>
2019-06-22 09:07:12 -0500

    Update plugin release to 1.2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learndash-muvi
 A WordPress LearnDash extension plugin that provides integration with the Muvi.com platform
+
+CHANGELOG
+
+1.2 - June 22, 2019
+- Fix: LearnDash 3.0 compatibility
--- learndash-muvi.php
+++ learndash-muvi.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: LearnDash Muvi Integration
  * Description: Add functions for integrating LearnDash with the Muvi platform
- * Version: 1.1
+ * Version: 1.2
  * Author: Abundant Designs LLC
  * Author URI: https://www.abundantdesigns.com/
  * License: GPLv2 or later
@@ -92,7 +92,7 @@ class LearnDash_Muvi {
 
         // Plugin version
         if ( ! defined( 'LEARNDASH_MUVI_VERSION' ) ) {
-            define( 'LEARNDASH_MUVI_VERSION', '1.0' );
+            define( 'LEARNDASH_MUVI_VERSION', '1.2' );
         }
 
         // Plugin text domain
